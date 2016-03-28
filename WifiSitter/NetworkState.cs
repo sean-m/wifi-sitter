05e266b2b2cf5701f4d49e8aac035ba028a6780c
Sean McArdle <sean.mcardle0@gmail.com>
2016-03-28 11:17:33 -0700

    Refactored classes into individual files in preparation for servicizing, Program.cs could get cluttered.

---
+++ WifiSitter/NetworkState.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace WifiSitter
+{
+    /// <summary>
+    /// Class used to track state of detected network adapters
+    /// </summary>
+    public class NetworkState
+    {
+        #region fields
+        private List<SitterNic> _nics;
+        private bool _checkNet;
+        private bool _netAvailable;
+        private bool _processingState;
+        #endregion // fields
+
+
+        #region constructor
+
+        public NetworkState() {
+            this.Nics = QueryNetworkAdapters();
+            Initialize();
+        }
+
+        public NetworkState(List<SitterNic> Nics) {
+            this.Nics = Nics;
+            Initialize();
+
+        }
+
+        private void Initialize() {
+            CheckNet = true;
+            _netAvailable = NetworkInterface.GetIsNetworkAvailable();
+            NetworkChange.NetworkAddressChanged += NetworkChange_NetworkAddressChanged; ;
+            NetworkChange.NetworkAvailabilityChanged += NetworkChange_NetworkAvailabilityChanged;
+
+            _processingState = true;
+        }
+
+        ~NetworkState() {
+            NetworkChange.NetworkAddressChanged -= NetworkChange_NetworkAddressChanged; ;
+            NetworkChange.NetworkAvailabilityChanged -= NetworkChange_NetworkAvailabilityChanged;
+        }
+
+        #endregion // constructor
+
+
+        #region methods
+
+        public void StateChecked() {
+            this.CheckNet = false;
+            this.ProcessingState = false;
+        }
+
+        public void UpdateNics(List<SitterNic> Nics) {
+            this.Nics = Nics;
+        }
+
+
+        internal static List<SitterNic> QueryNetworkAdapters() {
+            List<SitterNic> result = new List<SitterNic>();
+            foreach (var n in NetworkInterface.GetAllNetworkInterfaces().Where(x => (x.NetworkInterfaceType != NetworkInterfaceType.Loopback
+                                                                                  && x.NetworkInterfaceType != NetworkInterfaceType.Tunnel
+                                                                                  && !x.Description.ToLower().Contains("bluetooth")
+                                                                                  && !x.Description.StartsWith("Microsoft Wi-Fi Direct")  // TODO make this configurable via registry
+                                                                                  && !x.Description.StartsWith("VirtualBox Host")))) {
+                result.Add(new SitterNic(n));
+            }
+            return result;
+        }
+
+        #endregion // methods
+
+
+        #region properties
+
+        public bool EthernetUp {
+            get {
+                if (Nics == null) return false;
+
+                return Nics.Where(x => x.Nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                           .Any(x => x.Nic.OperationalStatus == OperationalStatus.Up);
+            }
+        }
+
+        public List<SitterNic> Nics {
+            get {
+                if (_nics == null) return new List<SitterNic>();
+                return _nics;
+            }
+            private set { _nics = value; }
+        }
+
+
+        public bool CheckNet {
+            get { return _checkNet; }
+            private set { _checkNet = value; }
+        }
+
+
+        public bool NetworkAvailable {
+            get { return _netAvailable; }
+            private set { _netAvailable = value; }
+        }
+
+
+        public bool ProcessingState {
+            get { return _processingState; }
+            set { _processingState = value; }
+        }
+
+        #endregion // properties
+
+
+        #region eventhandlers
+
+        private void NetworkChange_NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0}  Network availability changed.", DateTime.Now.ToString());
+            Console.ResetColor();
+            _netAvailable = NetworkInterface.GetIsNetworkAvailable();
+            _checkNet = true;
+        }
+
+        private void NetworkChange_NetworkAddressChanged(object sender, EventArgs e) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0}  Network address changed.", DateTime.Now.ToString());
+            Console.ResetColor();
+            _netAvailable = NetworkInterface.GetIsNetworkAvailable();
+            _checkNet = true;
+        }
+
+        #endregion // eventhandlers
+    }
+}
---
+++ WifiSitter/SitterNic.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace WifiSitter
+{
+    /// <summary>
+    /// Object that contains information from NetworkInterface objects
+    /// as well as netsh output (Admin State: Enabled/Disabled).
+    /// </summary>
+    public class SitterNic
+    {
+        private NetworkInterface _nic;
+        private bool _isEnabled;
+        private bool _isConnected;
+
+        #region constructor
+        public SitterNic(NetworkInterface Nic) {
+            this._nic = Nic;
+            _isEnabled = false;
+        }
+
+        public SitterNic(NetworkInterface Nic, bool IsEnabled) {
+            _nic = Nic;
+            _isEnabled = IsEnabled;
+        }
+        #endregion // constructor
+
+
+        #region properties
+
+        public NetworkInterface Nic {
+            get { return _nic; }
+        }
+
+        public bool IsEnabled {
+            get { return _isEnabled; }
+            set { _isEnabled = value; }
+        }
+
+
+        public string Name {
+            get { return Nic.Name; }
+        }
+
+
+        public string Description {
+            get { return Nic.Description; }
+        }
+
+
+        public string Id {
+            get { return Nic.Id; }
+        }
+
+
+        public bool IsConnected {
+            get { return _isConnected; }
+            set { _isConnected = value; }
+        }
+
+        #endregion // properties
+
+
+        #region methods
+
+        public void UpdateState(List<NetshInterface> NetshIfs) {
+            this.UpdateState(NetshIfs.Where(x => x.InterfaceName == Nic.Name).FirstOrDefault());
+        }
+
+        public void UpdateState(NetshInterface NetshIf) {
+            if (NetshIf == null) return;
+
+            if (Nic.Name == NetshIf.InterfaceName) {
+                this._isEnabled = NetshIf.AdminState == "Enabled";
+                this._isConnected = NetshIf.State == "Connected";
+            }
+        }
+
+        public bool Disable() {
+            // Release IP first and update NIC inforamtion so OperationalState reflects this
+            this.ReleaseIp();
+
+            // Disable interface
+            int exitCode = EnableDisableInterface(false);
+
+            var netsh = NetshHelper.GetInterfaces().Where(x => x.InterfaceName == this.Name).FirstOrDefault();
+
+            if (netsh != null) {
+                this.UpdateState(netsh);
+            }
+            else {
+                this._isEnabled = false;
+                this._isConnected = false;
+            }
+
+            return !IsEnabled;
+        }
+
+        public bool Enable() {
+            int exitCode = EnableDisableInterface(true);
+
+            var netsh = NetshHelper.GetInterfaces().Where(x => x.InterfaceName == this.Name).FirstOrDefault();
+
+            if (netsh != null) {
+                this.UpdateState(netsh);
+            }
+            else {
+                this._isEnabled = false;
+                this._isConnected = false;
+            }
+
+            return IsEnabled;
+        }
+
+        private int EnableDisableInterface(bool Enable) {
+            string state = Enable ? "ENABLED" : "DISABLED";
+
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "netsh.exe";
+            proc.StartInfo.Arguments = String.Format("interface set interface name=\"{0}\" admin={1}", this.Name, state);
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.Start();
+
+            while (!proc.HasExited) { System.Threading.Thread.Sleep(100); }
+
+            return proc.ExitCode;
+        }
+
+        private bool ReleaseIp() {
+            //ipconfig /release "Ethernet"
+
+            if (String.IsNullOrEmpty(this.Name)) { throw new ArgumentException("InterfaceName cannot be null or empty"); }
+
+            List<NetshInterface> results = new List<NetshInterface>();
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "ipconfig.exe";
+            proc.StartInfo.Arguments = String.Format("/release \"{0}\"", this.Name);
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.Start();
+
+            while (!proc.HasExited) { System.Threading.Thread.Sleep(100); }
+
+            return proc.ExitCode == 0;
+        }
+
+        #endregion // methods
+    }
+}
