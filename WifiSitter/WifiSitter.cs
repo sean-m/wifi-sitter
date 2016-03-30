0e7c0962e01b0950a6a9bafd87b2af2d9a4ad98d
Sean McArdle <sean.mcardle0@gmail.com>
2016-03-29 17:59:02 -0700

    Cleanup usings.

--- WifiSitter/WifiSitter.cs
+++ WifiSitter/WifiSitter.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.NetworkInformation;
 using System.Threading;
-using System.ServiceProcess;
-using System.Diagnostics;
-using Microsoft.Win32;
 
 namespace WifiSitter
 {
