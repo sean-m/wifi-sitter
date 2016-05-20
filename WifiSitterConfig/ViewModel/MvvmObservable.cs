fd319b62be34ab185534abb3f60915f0b5420cf3
Sean McArdle <sean.mcardle0@gmail.com>
2016-05-19 22:42:40 -0700

    Basic scaffolding for config GUI using MVVM model. Added the ManagedWifi API source from codeplex (MIT License).

---
+++ WifiSitterConfig/ViewModel/MvvmObservable.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace WifiSitterConfig.ViewModel
+{
+    public class MvvmObservable : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        internal virtual void OnPropertyChanging(string propertyName) {
+            PropertyChangingEventHandler handler = this.PropertyChanging;
+            if (handler != null)
+                handler(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        #endregion // INotifyPropertyChanging Members
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal virtual void OnPropertyChanged(string propertyName) {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+}
