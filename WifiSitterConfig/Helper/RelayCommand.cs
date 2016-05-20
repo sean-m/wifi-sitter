f75a6a41411bbd9e91401c71cd31fa311a022fe9
Sean McArdle <sean.mcardle0@gmail.com>
2016-05-19 23:59:17 -0700

    Wrong namespace.

--- WifiSitterConfig/Helper/RelayCommand.cs
+++ WifiSitterConfig/Helper/RelayCommand.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.Windows.Input;
 
-namespace WifiSitter.Helper
+namespace WifiSitterConfig.Helper
 {
     /// <summary>
     /// A command whose sole purpose is to 
