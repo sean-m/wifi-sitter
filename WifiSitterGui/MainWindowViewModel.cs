5cba900d916bf3429a696eccfb8956e46257df11
Sean McArdle <sean.mcardle0@gmail.com>
2016-11-15 14:04:48 -0800

    TrayIconControl is a hidden window launched by default that hosts the tray icon and stores viewmodel state for the MainWindow.

---
+++ WifiSitterGui/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WifiSitterGui
+{
+    class MainWindowViewModel : MvvmObservable
+    {
+        #region fields
+        #endregion  // fields
+
+        #region constructor
+
+        public MainWindowViewModel () {
+
+        }
+
+        #endregion  // constructor
+
+        #region properties
+        #endregion  // properties
+
+        #region methods
+        #endregion  // methods
+
+        #region eventhandlers
+        #endregion  // methods
+    }
+}
---
+++ WifiSitterGui/TrayIconControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WifiSitterGui
+{
+    /// <summary>
+    /// Interaction logic for TrayIconControl.xaml
+    /// </summary>
+    public partial class TrayIconControl : Window
+    {
+        private MainWindowViewModel _windowVm;
+
+        public TrayIconControl() {
+            InitializeComponent();
+
+            _windowVm = new MainWindowViewModel();
+
+            var statusGui = new MainWindow();
+            statusGui.DataContext = _windowVm;
+            statusGui.Show();
+        }
+    }
+}
