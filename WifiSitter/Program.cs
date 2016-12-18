e2d59dcf57eb6cb0e17b3f0d28f846d60fd2448e
Sean McArdle <sean.mcardle0@gmail.com>
2016-12-17 17:11:38 -0800

    First pass at allowing only one application instance at a time.

--- WifiSitter/Program.cs
+++ WifiSitter/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace WifiSitter
+﻿using System.Diagnostics;
+using System.Linq;
+using System;
+
+namespace WifiSitter
 {
     class Program
     {
         static void Main(string[] args) { // entry point for cmd
-            (new WifiSitter()).Run(args);
+            var isRunning = Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1;
+            if (isRunning) {
+                Console.WriteLine("WifiSitter already running...\nQuiting in 10 seconds.");
+                System.Threading.Thread.Sleep(10 * 1000);
+                Environment.Exit(7);
+            }
+            else {
+                (new WifiSitter()).Run(args);
+            }
         }
     }
 }
