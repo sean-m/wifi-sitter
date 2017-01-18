2f7a0b6a1a3fedb7eb12f0d5a62e8c99afe1b7ba
Sean McArdle <sean.mcardle0@gmail.com>
2017-01-18 14:59:43 -0800

    Only prevent initializing wifisitter if another copy is running and trying to start in "console" or "service" mode.

--- WifiSitter/Program.cs
+++ WifiSitter/Program.cs
@@ -9,14 +9,16 @@ namespace WifiSitter
     class Program
     {
         static void Main(string[] args) { // entry point for cmd
+            Configuration.SetOptions(args);
+
             var isRunning = Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1;
-            if (isRunning) {
-                Console.WriteLine("WifiSitter already running...\nQuiting in 10 seconds.");
-                System.Threading.Thread.Sleep(10 * 1000);
-                Environment.Exit(7);
+            string mode = (string)Configuration.GetOption("operating_mode");
+            if (isRunning && (mode == "console" || mode == "service")) {
+                    Console.WriteLine("WifiSitter already running...\nQuiting in 10 seconds.");
+                    System.Threading.Thread.Sleep(10 * 1000);
+                    Environment.Exit(7);
             }
             else {
-                Configuration.SetOptions(args);
                 (new WifiSitter()).Run(args);
             }
         }
